f28da6ada9ee897eaf391db8bc03b92dda539f5f
José Erico <jose.ericos.ls@gmail.com>
2024-09-17 18:59:56 -0300

    Criando cabeçalho e rodapé

--- index.php
+++ index.php
@@ -1,25 +1,23 @@
 <?php
 require_once("globals.php");
-require_once("db.php");
+
 ?>
+
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MovieStar</title>
     <link rel="short icon" href="<?= $BASE_URL ?>img/moviestar.ico">
-    <!-- Bootstrap -->
+    <!-- BOOTSTRAP  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap.css" integrity="sha512-VcyUgkobcyhqQl74HS1TcTMnLEfdfX6BbjhH8ZBjFU9YTwHwtoRtWSGzhpDVEJqtMlvLM2z3JIixUOu63PNCYQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- Font Awesome -->
+    <!-- FONTE AWESOME -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- CSS do Projeto -->
-    <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
 </head>
-<!-- BOOTSTRAP JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/js/bootstrap.js" integrity="sha512-lsA4IzLaXH0A+uH6JQTuz6DbhqxmVygrWv1CpC/s5vGyMqlnP0y+RYt65vKxbaVq+H6OzbbRtxzf+Zbj20alGw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 <body>
     <header>
@@ -32,14 +30,14 @@ require_once("db.php");
                 <i class="fas fa-bars"></i>
             </button>
             <form action="" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
-                <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Busca Filmes" aria-label="Search">
+                <input type="text" name="q" id="search" class="form control mr-sm-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
                 <button class="btn my-2 my-sm-0" type="submit">
                     <i class="fas fa-search"></i>
                 </button>
             </form>
             <div class="collapse navbar-collapse" id="navbar">
                 <ul class="navbar-nav">
-                    <li class="nav-item">
+                    <li class="nav item">
                         <a href="<?= $BASE_URL ?>auth.php" class="nav-link">Entrar / Cadastrar</a>
                     </li>
                 </ul>
@@ -59,20 +57,22 @@ require_once("db.php");
                     <a href="#"><i class="fab fa-instagram"></i></a>
                 </li>
                 <li>
-                    <a href=" #"><i class="fab fa-youtube"></i></a>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
                 </li>
             </ul>
         </div>
         <div id="footer-links-container">
             <ul>
                 <li><a href="#">Adicionar filme</a></li>
-                <li><a href="#">Adicionar crítica</a></li>
-                <li><a href="#">Entrar / Registra</a></li>
+                <li><a href="#">Adicionar critica</a></li>
+                <li><a href="#">Entrar / Registrar</a></li>
             </ul>
         </div>
-        <p>&copy; 2020 er1cos</p>
+        <p>&copy; 2024 er1cos</p>
     </footer>
-
+    <!-- BOOTSTRAP JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/js/bootstrap.js" integrity="sha512-lsA4IzLaXH0A+uH6JQTuz6DbhqxmVygrWv1CpC/s5vGyMqlnP0y+RYt65vKxbaVq+H6OzbbRtxzf+Zbj20alGw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
 
 </html>
