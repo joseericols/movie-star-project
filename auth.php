6cca4a45ee8a7227f0ad7de9f8da92fa3fe8a81c
José Erico <jose.ericos.ls@gmail.com>
2024-09-30 12:29:51 -0300

    Separação dos templates

--- auth.php
+++ auth.php
@@ -2,7 +2,7 @@
 include_once("templates/header.php")
 ?>
 <div id="main-container" class="container-fluid">
-    <h1>Corpo do site</h1>
+    <h1>Autenticação</h1>
 </div>
 <?php
 include_once("templates/footer.php")
