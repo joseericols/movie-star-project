368cb52ccbd53854799be8f9a460ed2a1dc79ee8
José Erico <jose.ericos.ls@gmail.com>
2024-10-03 20:17:45 -0300

    Preparando os dados do usuario

--- auth_process.php
+++ auth_process.php
@@ -28,7 +28,21 @@ if ($type == "register") {
 
             //Verificar se o e-mail já está cadastrado no sistema.
             if ($userDao->findByEmail($email) === false) {
-                echo "Nenhum usuário foi encontrado";
+                $user = new User();
+
+                //criação de token e senha.
+                $userToken = $user->generateToken();
+                $finalPassword = $user->generatePassword($password);
+
+                $user->name = $name;
+                $user->lastname = $lastname;
+                $user->email = $email;
+                $user->password = $finalPassword;
+                $user->token = $userToken;
+
+                $auth = True;
+
+                $userDao->create($user, $auth);
             }
         } else {
             //Enviar mensagem de erro se as senhas não forem iguais.
